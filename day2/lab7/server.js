4a96fe2ee1043bebf861468b863242bcaa8e2ef5
GREY7 <GREY7@ptc.local>
2016-12-07 11:16:01 +0200

    Added boostrap template

--- day2/lab7/server.js
+++ day2/lab7/server.js
@@ -7,15 +7,19 @@ app.listen(3000);
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var MongoStore = require('connect-mongo/es5')(session);
-var fs = require('fs');
 var Db = require('mongodb').Db;
 var Server = require('mongodb').Server;
+var ObjectID = require('mongodb').ObjectID;
 
 var db = new Db('tutor',
     new Server("localhost", 27017, {safe: true},
         {auto_reconnect: true}, {}));
+
 db.open(function(){
     console.log("mongo db is opened!");
+    db.collection('notes', function(error, notes) {
+        db.notes = notes;
+    });
 });
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -29,56 +33,28 @@ app.use(session({
     saveUninitialized: true
 }));
 
-// app.get("/notes", function(req,res) {
-//     res.send(req.session.notes||[]);
-// });
 app.get("/notes", function(req,res) {
-    fs.readFile("notes.json", function(err, result) {
-        if (result) {
-            result = ""+result; // convert Object to String
-            //remove last \n in file
-            result = result.substring(0, result.length - 1);
-            result = "["+result+"]";
-            result = result.split("\n").join(",");
-            res.send(result);
-        } else {
-            res.end();
-        }
+    db.notes.find(req.query).toArray(function (err, items) {
+        res.send(items);
     });
 });
-//
-// app.post("/notes", function(req, res) {
-//     if (!req.session.notes) {
-//         req.session.notes = [];
-//         req.session.last_note_id = 0;
-//     }
-//     var note = req.body;
-//     note.id = req.session.last_note_id;
-//     req.session.last_note_id++;
-//     req.session.notes.push(note);
-//     res.end();
-// });
 
 app.post("/notes", function(req, res) {
     var note = req.body;
-    var noteText = JSON.stringify(note)+"\n";
-    fs.appendFile("notes.json", noteText, function(err) {
-        if (err) console.log("something is wrong");
-        res.end();
-    });
+    db.notes.insert(note);
+    res.end();
 });
 
 app.delete("/notes", function(req,res) {
-    var id = req.query.id;
-    var notes = req.session.notes||[];
-    var updatedNotesList = [];
-    for (var i=0;i<notes.length;i++) {
-        if (notes[i].id != id) {
-            updatedNotesList.push(notes[i]);
+    var id = new ObjectID(req.query.id);
+    db.notes.remove({_id: id}, function(err){
+        if (err) {
+            console.log(err);
+            res.send("Failed");
+        } else {
+            res.send("Success");
         }
-    }
-    req.session.notes = updatedNotesList;
-    res.end();
+    })
 });
 
 app.put("/notes", function(req,res) {
